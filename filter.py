d4359183a012fa64a1f48a5c068da75685e70d10
Laugh1Tale <saharus@outlook.com>
2021-12-02 15:46:47 +0500

    Documentation has been written. Added some tests.

--- filter.py
+++ filter.py
@@ -1,30 +1,168 @@
+import sys
 from PIL import Image
 import numpy as np
+import argparse
+import datetime
 
 
-def convert_image_to_mosaic(image, size, gradation_step):
-    for x in range(0, len(image), size):
-        for y in range(0, len(image[0]), size):
-            image[x:x + size, y:y + size] = get_average_brightness(
-                image[x:x + size, y:y + size], size, gradation_step)
-    return image
+def create_namespace():
+    """
+    Сreates and returns a namespace with specific arguments.
+    """
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-i', '--image', default=None, metavar='',
+                            help='set image to transform into a mosaic in grayscale')
+    arg_parser.add_argument('-r', '--result', default='res.jpg', metavar='',
+                            help='set name with which the result will be saved')
+    return arg_parser.parse_args(sys.argv[1:])
 
 
-def get_average_brightness(block, size, gradation_step):
-    average_color = (block[:size, :size].sum() / 3) // size ** 2
-    return int(average_color // gradation_step) * gradation_step
+def open_image(image_name):
+    """
+    Receives the name of the image and tries to open it, handles exceptions.
+
+    :param image_name: name of the image in the current directory
+    :type image_name: str
+    """
+    while True:
+        try:
+            image = Image.open(image_name)
+            return image
+        except Exception as e:
+            register_an_error(e)
+            print('Incorrect input.')
+            print('Enter the image name in the current directory '
+                  'or specify the full path to your file. Write "exit" if you want to exit.')
+            image_name = input()
+            if image_name == 'exit':
+                exit()
 
 
-def main():
-    image_file = Image.open(input("Введите имя файла, которое хотите конвертировать: "))
-    block_size = int(input("Введите размер блока: "))
-    gradations_count = int(input("Введите количество градаций серого: "))
-    image = np.array(image_file)
-    gradation_step = 255 // gradations_count
+def register_an_error(error):
+    """
+    Receives an error and logs it to a file log.txt, handles exceptions.
 
-    res = Image.fromarray(convert_image_to_mosaic(image, block_size, gradation_step))
-    res.save(input("Введите имя файла, в которой хотите сохранить результат: "))
+    :param error: name of the image in the current directory
+    :type error: Exception
+    """
+    try:
+        with open('./log.txt', 'a') as file:
+            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            file.write("[{}] - {}\n".format(now, error))
+    except Exception as e:
+        with open('./log.txt', 'w+') as file:
+            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            file.write("[{}] - {}\n".format(now, e))
+            file.write("[{}] - {}\n".format(now, error))
+
+
+def set_grayscale():
+    """
+    Requests grayscale input and returns, handles exceptions.
+
+    :rtype: int
+    """
+    while True:
+        try:
+            grayscale = int(input('Set grayscale.\n'))
+            break
+        except Exception as e:
+            register_an_error(e)
+            print('Incorrect input. Please enter grayscale in correct format.')
+    return grayscale
+
+
+def set_mosaic_dimensions():
+    """
+    Requests mosaic height and width input and returns, handles exceptions.
+
+    :rtype: object
+    """
+    while True:
+        try:
+            height = int(input('Set the mosaic height.\n'))
+            break
+        except Exception as e:
+            register_an_error(e)
+            print('Incorrect input. Please enter mosaic height in correct format.')
+    while True:
+        try:
+            width = int(input('Set the mosaic width.\n'))
+            break
+        except Exception as e:
+            register_an_error(e)
+            print('Incorrect input. Please enter mosaic width in correct format.')
+    return height, width
+
+
+def get_color(brightness, step):
+    """
+    Receives the brightness of the mosaic fragment and grayscale,
+    and returns the color to which the fragment will be converted.
+
+    :param brightness: brightness of the mosaic fragment
+    :type brightness: int
+    :param step: grayscale
+    :type step: int
+
+    >>> get_color(49, 50)
+    0
+
+    >>> get_color(50, 50)
+    50
+
+    >>> get_color(51, 50)
+    50
+    """
+    return int(brightness // step) * step
+
+
+def replace_with_gray(dimensions, image, step):
+    """
+    Receives the mosaic dimensions, image and grayscale,
+    processes and returns the image according to the specified parameters.
+
+    :param dimensions: mosaic dimensions (height, width)
+    :type dimensions: tuple(int, int)
+    :param image: image
+    :type image: Image
+    :param step: grayscale
+    :type step: int
+    """
+    array = np.array(image)
+    height = dimensions[0]
+    width = dimensions[1]
+    for x in range(0, len(array), height):
+        for y in range(0, len(array[1]), width):
+            # find out the average brightness
+            average_brightness = np.sum(array[x: x + height, y: y + width]) // (height * width * 3)
+            # bring the color of average brightness to the step in increments of 50
+            color = get_color(average_brightness, step)
+            # paint the cell into mosaics in the resulting color
+            array[x: x + height, y: y + width] = np.full(3, color)
+    return Image.fromarray(array)
+
+
+def save_image(result_name, image):
+    """
+    Receives the processed image and the image name, saves it, handles exceptions.
+
+    :param result_name: the name under which the image should be saved
+    :type result_name: str
+    :param image: received  processed image
+    :type image: Image
+    """
+    while True:
+        try:
+            return image.save(result_name)
+        except Exception as e:
+            register_an_error(e)
+            print('Enter a different name for the output image or specify the correct file format.')
+            result_name = input()
 
 
 if __name__ == '__main__':
-    main()
+    namespace = create_namespace()
+    img = open_image(namespace.image)
+    processed_image = replace_with_gray(set_mosaic_dimensions(), img, set_grayscale())
+    save_image(namespace.result, processed_image)
--- filter_with_filename.py
+++ filter_with_filename.py
@@ -1,30 +1,99 @@
+import sys
 from PIL import Image
 import numpy as np
+import argparse
+import datetime
 
 
-def convert_image_to_mosaic(image, size, gradation_step):
-    for x in range(0, len(image), size):
-        for y in range(0, len(image[0]), size):
-            image[x:x + size, y:y + size] = get_average_brightness(
-                image[x:x + size, y:y + size], size, gradation_step)
-    return image
+def create_namespace():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-i', '--image', default=None, metavar='',
+                            help='set image to transform into a mosaic in grayscale')
+    arg_parser.add_argument('-r', '--result', default='res.jpg', metavar='',
+                            help='set name with which the result will be saved')
+    return arg_parser.parse_args(sys.argv[1:])
 
 
-def get_average_brightness(block, size, gradation_step):
-    average_color = (block[:size, :size].sum() / 3) // size ** 2
-    return int(average_color // gradation_step) * gradation_step
+def open_image(image_name):
+    while True:
+        try:
+            img = Image.open(image_name)
+            return np.array(img)
+        except Exception as e:
+            register_an_error(e)
+            print('Incorrect input.')
+            print('Enter the image name in the current directory '
+                  'or specify the full path to your file. Write "exit" if you want to exit.')
+            image_name = input()
+            if image_name == 'exit':
+                exit()
 
 
-def main():
-    image_file = Image.open("scale1200.jpg")
-    block_size = 10
-    gradations_count = 50
-    image = np.array(image_file)
-    gradation_step = 255 // gradations_count
+def register_an_error(error):
+    try:
+        with open('./log.txt', 'a') as file:
+            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            file.write("[{}] - {}\n".format(now, error))
+    except Exception as e:
+        with open('./log.txt', 'w+') as file:
+            now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            file.write("[{}] - {}\n".format(now, e))
+            file.write("[{}] - {}\n".format(now, error))
 
-    res = Image.fromarray(convert_image_to_mosaic(image, block_size, gradation_step))
-    res.save("res_new.jpg")
+
+def set_grayscale():
+    while True:
+        try:
+            return int(input('Set grayscale.\n'))
+        except Exception as e:
+            register_an_error(e)
+            print('Incorrect input. Please enter grayscale in correct format.')
+
+
+def set_mosaic_dimensions():
+    while True:
+        try:
+            height = int(input('Set the mosaic height.\n'))
+            break
+        except Exception as e:
+            register_an_error(e)
+            print('Incorrect input. Please enter mosaic height in correct format.')
+    while True:
+        try:
+            width = int(input('Set the mosaic width.\n'))
+            break
+        except Exception as e:
+            register_an_error(e)
+            print('Incorrect input. Please enter mosaic width in correct format.')
+    return height, width
+
+
+def replace_with_gray(dimensions, array, step):
+    height = dimensions[0]
+    width = dimensions[1]
+    for x in range(0, len(array), height):
+        for y in range(0, len(array[1]), width):
+            # find out the average brightness
+            average_brightness = np.sum(array[x: x + height, y: y + width]) // (height * width * 3)
+            # bring the color of average brightness to the step in increments of 50
+            color = int(average_brightness // step) * step
+            # paint the cell into mosaics in the resulting color
+            array[x: x + height, y: y + width] = np.full(3, color)
+    return Image.fromarray(array)
+
+
+def save_image(result_name, array):
+    while True:
+        try:
+            return array.save(result_name)
+        except Exception as e:
+            register_an_error(e)
+            print('Enter a different name for the output image or specify the correct file format.')
+            result_name = input()
 
 
 if __name__ == '__main__':
-    main()
+    namespace = create_namespace()
+    arr = open_image(namespace.image)
+    image = replace_with_gray((10, 10), arr, 50)
+    save_image(namespace.result, image)
